14e5c1621ccafc46416b19dff45c96a05e5cd710
Raycoms <ray.neiheiser@gmail.com>
2025-06-02 11:51:35 +0200

    Add

---
+++ src/main/java/com/ldtteam/desertrevival/DesertRevival.java
@@ -0,0 +1,12 @@
+package com.ldtteam.desertrevival;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("desertrevival")
+public class DesertRevival
+{
+    public DesertRevival()
+    {
+
+    }
+}
